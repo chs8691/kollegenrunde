24cb1f883d7bfbe61ef3b7c368cfa34a4e655cfc
Christian <chs8691@mailbox.org>
2020-09-29 23:47:20 +0200

    German number formatting

--- redaktion/publish.sh
+++ redaktion/publish.sh
@@ -2,4 +2,4 @@
 
 echo Publish to Uberspace...
 
-rsync -av --delete /home/chris/hugo/kollegenrunde/public/ kollegen@despina.uberspace.de:html/kollegenrunde
+rsync -avv --delete /home/chris/hugo/kollegenrunde/public/ kollegen@despina.uberspace.de:html/kollegenrunde
